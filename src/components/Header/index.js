e59a00a917ff706048fb361f723f00041a721fc9
sali6798 <shaidee@alingcastre.net>
2020-05-07 22:35:05 -0700

    added instruction

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,10 @@ function Header() {
     return (
         <header>
             <h1>Employee Directory</h1>
+            <p>
+                Start typing in a name in the search bar below or sort the employees in<br />
+                alphabetical order by clicking on the arrow button next to Name
+            </p>
         </header>
     );
 }
